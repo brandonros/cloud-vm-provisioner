d19ea86bde87be7a7e008b4b01ee81c49ffaea9a
Brandon Ros <brandonros1@gmail.com>
2025-04-04 08:26:56 -0400

    arm64 support

--- terraform/01-k3s/modules/00-dependencies/setup.sh
+++ terraform/01-k3s/modules/00-dependencies/setup.sh
@@ -23,9 +23,22 @@ sudo apt-get -y install acl htop psmisc netcat-traditional
 # Remove unused packages
 sudo apt-get -y autoremove
 
+# Detect architecture
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH_K9S="amd64"
+    ARCH_HELM="amd64"
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    ARCH_K9S="arm64"
+    ARCH_HELM="arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 # Install k9s
 if ! command -v k9s &> /dev/null; then
-    wget https://github.com/derailed/k9s/releases/download/v0.40.5/k9s_linux_amd64.deb
+    wget https://github.com/derailed/k9s/releases/download/v0.40.5/k9s_linux_${ARCH_K9S}.deb
     sudo apt install -y ./k9s_linux_*.deb
     rm k9s_linux_*.deb
 else
@@ -34,10 +47,10 @@ fi
 
 # Install helm
 if ! command -v helm &> /dev/null; then
-    wget https://get.helm.sh/helm-v3.17.2-linux-amd64.tar.gz
-    tar -xf helm-v3.17.2-linux-amd64.tar.gz
-    sudo install -m 755 linux-amd64/helm /usr/local/bin/helm
-    rm -rf linux-amd64
+    wget https://get.helm.sh/helm-v3.17.2-linux-${ARCH_HELM}.tar.gz
+    tar -xf helm-v3.17.2-linux-${ARCH_HELM}.tar.gz
+    sudo install -m 755 linux-${ARCH_HELM}/helm /usr/local/bin/helm
+    rm -rf linux-${ARCH_HELM}
 else
     echo "helm is already installed"
 fi
