d109ada35690358752da48da4cda2363b3293071
Brandon Ros <brandonros1@gmail.com>
2025-04-05 17:39:23 -0400

    lgtm stack?

--- terraform/02-platform/main.tf
+++ terraform/02-platform/main.tf
@@ -35,6 +35,11 @@ provider "helm" {
   }
 }
 
+module "metrics_server" {
+  source   = "./modules/helm-release"
+  manifest = yamldecode(file("${path.module}/manifests/metrics-server.yaml"))
+}
+
 module "gateway_api" {
   source = "./modules/gateway-api"
   instance_username = data.terraform_remote_state.vm.outputs.instance_username
@@ -42,14 +47,8 @@ module "gateway_api" {
   instance_ssh_port = data.terraform_remote_state.vm.outputs.instance_ssh_port
 }
 
-module "metrics_server" {
-  depends_on = [module.gateway_api]
-  source   = "./modules/helm-release"
-  manifest = yamldecode(file("${path.module}/manifests/metrics-server.yaml"))
-}
-
 module "cert_manager" {
-  depends_on = [module.metrics_server]
+  depends_on = [module.gateway_api]
   source   = "./modules/helm-release"
   manifest = yamldecode(file("${path.module}/manifests/cert-manager.yaml"))
 }
@@ -61,7 +60,31 @@ module "traefik" {
 }
 
 module "rabbitmq" {
-  depends_on = [module.traefik]
   source   = "./modules/helm-release"
   manifest = yamldecode(file("${path.module}/manifests/rabbitmq.yaml"))
+}
+
+module "loki" {
+  source   = "./modules/helm-release"
+  manifest = yamldecode(file("${path.module}/manifests/loki.yaml"))
+}
+
+module "grafana" {
+  source   = "./modules/helm-release"
+  manifest = yamldecode(file("${path.module}/manifests/grafana.yaml"))
+}
+
+module "tempo" {
+  source   = "./modules/helm-release"
+  manifest = yamldecode(file("${path.module}/manifests/tempo.yaml"))
+}
+
+module "mimir" {
+  source   = "./modules/helm-release"
+  manifest = yamldecode(file("${path.module}/manifests/mimir.yaml"))
+}
+
+module "alloy" {
+  source   = "./modules/helm-release"
+  manifest = yamldecode(file("${path.module}/manifests/alloy.yaml"))
 }
