213971780e326cae7fd19f1851ddf9dcc469a2c9
Brandon Ros <brandonros1@gmail.com>
2025-07-12 13:51:49 -0400

    do not hardcode vultr

--- terraform/02-platform/modules/gateway-api/main.tf
+++ terraform/02-platform/modules/gateway-api/main.tf
@@ -5,12 +5,12 @@ variable "instance_username" {
 
 variable "instance_ip" {
   type = string
-  description = "IP address of the Vultr instance"
+  description = "IP address of the VM"
 }
 
 variable "instance_ssh_port" {
   type = number
-  description = "SSH port of the Vultr instance"
+  description = "SSH port of the VM"
 }
 
 resource "null_resource" "install_gateway_api" {
